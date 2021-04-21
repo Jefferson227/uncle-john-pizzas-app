92f8ed1c74a5a7c3f15d20352e717410c3aef02a
Jefferson Matheus <jefferson227@gmail.com>
2021-04-21 19:08:42 -0300

    Add list items in the side menu

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './SideMenu.css';
-import sideMenuPhoto from '../../assets/img/sidemenu-header-photo.png';
-import { IonMenu, IonHeader, IonToolbar, IonContent, IonList, IonItem } from '@ionic/react';
+import SideMenuPhoto from '../../assets/img/sidemenu-header-photo.png';
+import { IonMenu, IonHeader, IonToolbar, IonContent, IonList, IonItem, IonIcon } from '@ionic/react';
+import ProfileIcon from '../../assets/img/profile-icon.svg';
+import PaymentMethodIcon from '../../assets/img/payment-method-icon.svg';
+import OrderHistoryIcon from '../../assets/img/order-history-icon.svg';
+import AddressesIcon from '../../assets/img/addresses-icon.svg';
+import HelpCenterIcon from '../../assets/img/help-center-icon.svg';
 
 interface SideMenuProps {
   contentId: string;
@@ -13,19 +18,38 @@ const SideMenu: React.FC<SideMenuProps> = ({contentId}) => {
       <IonHeader>
         <IonToolbar>
           <div className="sidemenu-header">
-            <div className="sidemenu-photo"><img className="sidemenu-profile-pic" src={sideMenuPhoto} /></div>
+            <div className="sidemenu-photo"><img className="sidemenu-profile-pic" src={SideMenuPhoto} /></div>
             <div className="sidemenu-name">Jaykey Del Mar</div>
             <div className="sidemenu-email">janedone@gmail.com</div>
           </div>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
-        <IonList>
-          <IonItem>Menu Item</IonItem>
-          <IonItem>Menu Item</IonItem>
-          <IonItem>Menu Item</IonItem>
-          <IonItem>Menu Item</IonItem>
-          <IonItem>Menu Item</IonItem>
+      <IonContent className="sidemenu-content">
+        <IonList className="sidemenu-list">
+          <IonItem className="sidemenu-list-item" lines="none">
+            <IonIcon src={ProfileIcon}></IonIcon>
+            <span className="sidemenu-item-text">Profile</span>
+          </IonItem>
+
+          <IonItem className="sidemenu-list-item" lines="none">
+            <IonIcon src={PaymentMethodIcon}></IonIcon>
+            <span className="sidemenu-item-text">Payment Method</span>
+          </IonItem>
+
+          <IonItem className="sidemenu-list-item" lines="none">
+            <IonIcon src={OrderHistoryIcon}></IonIcon>
+            <span className="sidemenu-item-text">Order History</span>
+          </IonItem>
+
+          <IonItem className="sidemenu-list-item" lines="none">
+            <IonIcon src={AddressesIcon}></IonIcon>
+            <span className="sidemenu-item-text">Addresses</span>
+          </IonItem>
+
+          <IonItem className="sidemenu-list-item" lines="none">
+            <IonIcon src={HelpCenterIcon}></IonIcon>
+            <span className="sidemenu-item-text">Help Center</span>
+          </IonItem>
         </IonList>
       </IonContent>
     </IonMenu>
