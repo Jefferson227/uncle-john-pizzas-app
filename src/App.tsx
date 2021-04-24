200383117437689bf13e96297d2aa392258468d6
Jefferson Matheus <jefferson227@gmail.com>
2021-04-24 17:49:30 -0300

    Remove ununsed reference

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { IonReactRouter } from '@ionic/react-router';
 import { setupConfig } from '@ionic/react';
 import Home from './pages/Home';
 import NavBar from './components/NavBar/NavBar';
-import DropDownMenu from './components/DropDownMenu/DropDownMenu';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
