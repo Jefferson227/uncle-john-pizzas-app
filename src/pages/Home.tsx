0fe81d70fa61a4a93a9d214b513a24b826b4b1b3
Jefferson Matheus <jefferson227@gmail.com>
2021-04-24 18:23:35 -0300

    Add header in the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,17 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <div className="home-padding-top"></div>
+
+        <section className="home-header">
+          <div className="home-greeting">Hi Jaykey!</div>
+
+          <div className="home-message">What <strong>pizza</strong> do you want to try today?</div>
+        </section>
       </IonContent>
     </IonPage>
   );
