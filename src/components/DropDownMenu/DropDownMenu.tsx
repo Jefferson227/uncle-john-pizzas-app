32c16c3110ffc2df0764c3bcb3f9a65d9a72c31f
Jefferson Matheus <jefferson227@gmail.com>
2021-04-24 12:16:35 -0300

    Implement dropdown list content partially

--- src/components/DropDownMenu/DropDownMenu.tsx
+++ src/components/DropDownMenu/DropDownMenu.tsx
@@ -1,4 +1,4 @@
-import { IonApp, IonRouterOutlet } from '@ionic/react';
+import { IonContent, IonList, IonItem } from '@ionic/react';
 import './DropDownMenu.css';
 
 interface DropDownMenuProps {
@@ -9,7 +9,15 @@ const DropDownMenu: React.FC<DropDownMenuProps> = ({labelId}) => (
   <section>
     <input type="checkbox" id={labelId} className="dropdown-menu-checkbox" />
 
-    <div className="dropdown-menu-content"></div>
+    <IonContent className="dropdown-menu-content">
+      <IonList className="dropdown-menu-list">
+        <IonItem className="dropdown-menu-list-item" lines="full">Current Location</IonItem>
+        <IonItem className="dropdown-menu-list-item" lines="full">Home</IonItem>
+        <IonItem className="dropdown-menu-list-item" lines="full">Other</IonItem>
+        <IonItem className="dropdown-menu-list-item" lines="full">Work</IonItem>
+        <IonItem className="dropdown-menu-list-item" lines="full">Add a new address</IonItem>
+      </IonList>
+    </IonContent>
   </section>
 );
 
