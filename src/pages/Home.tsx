4998513e460dfaaa987c1c6968f3b691c6b6c45f
Jefferson Matheus <jefferson227@gmail.com>
2021-04-29 23:30:04 -0300

    Start implementing reorder card on home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Home.css';
+import ThinCrustPizzaPhoto from '../assets/img/thin-crust-pizza-photo.png';
 
 const Home: React.FC = () => {
   return (
@@ -11,6 +12,18 @@ const Home: React.FC = () => {
           <div className="home-greeting">Hi Jaykey!</div>
 
           <div className="home-message">What <strong>pizza</strong> do you want to try today?</div>
+        </section>
+
+        <section className="home-reorder-container">
+          <img src={ThinCrustPizzaPhoto} alt="Thin Crust Pizza"/>
+
+          <div className="home-reorder-title">Order again?</div>
+
+          <div className="home-reorder-ingredients">Small, Thin Crust, Tomatoes, Basil, Cheese</div>
+
+          <div className="home-reorder-price">$12</div>
+
+          <button className="home-add-to-cart-button">Add To Cart</button>
         </section>
       </IonContent>
     </IonPage>
