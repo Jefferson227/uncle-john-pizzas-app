7f6e2327ad0cd38a79165c38f107cc2648f2712e
Jefferson Matheus <jefferson227@gmail.com>
2021-07-16 00:04:53 -0300

    Add NavBar in the ChooseSize page

--- src/pages/ChooseSize/ChooseSize.tsx
+++ src/pages/ChooseSize/ChooseSize.tsx
@@ -1,10 +1,13 @@
 import { IonContent, IonPage } from '@ionic/react';
+import NavBar from '../../components/NavBar/NavBar';
 import './ChooseSize.css';
 
 const ChooseSize: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
+        <NavBar />
+
         <p className="init">Hi, this is the sketch of the 'choose size' page.</p>
       </IonContent>
     </IonPage>
