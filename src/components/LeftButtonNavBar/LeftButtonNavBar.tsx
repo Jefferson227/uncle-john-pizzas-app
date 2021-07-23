34134ba3afdb32103593267af4f6ec07aa70c9bb
Jefferson Matheus <jefferson227@gmail.com>
2021-07-22 22:50:33 -0300

    Adjust size of back button

--- src/components/LeftButtonNavBar/LeftButtonNavBar.tsx
+++ src/components/LeftButtonNavBar/LeftButtonNavBar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MenuIcon from '../../assets/img/menu-icon.svg';
 import ArrowLeftIcon from '../../assets/img/arrow-left-icon.svg';
 import { IonMenuButton, IonIcon, IonBackButton } from '@ionic/react';
+import './LeftButtonNavBar.css';
 
 interface LeftButtonNavBarProps {
   type: string;
@@ -10,20 +11,20 @@ interface LeftButtonNavBarProps {
 const LeftButtonNavBar: React.FC<LeftButtonNavBarProps> = ({type}) => {
   if (type === 'menu')
     return (
-      <IonMenuButton className="ion-menu-button">
+      <IonMenuButton className="ion-navbar-button">
         <IonIcon src={MenuIcon}></IonIcon>
       </IonMenuButton>
     );
 
   if (type === 'back')
     return (
-      <IonBackButton className="ion-menu-button" defaultHref="/">
+      <IonBackButton className="ion-navbar-button ion-back-button" defaultHref="/">
         <IonIcon src={ArrowLeftIcon}></IonIcon>
       </IonBackButton>
     );
 
   return (
-    <IonMenuButton className="ion-menu-button">
+    <IonMenuButton className="ion-navbar-button">
       <IonIcon src={MenuIcon}></IonIcon>
     </IonMenuButton>
   );
