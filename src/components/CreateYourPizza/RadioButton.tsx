a583560e98048093ff14a9b12d034a0d621ff266
Jefferson Matheus <jefferson227@gmail.com>
2021-08-11 22:41:41 -0300

    Remove unnecessary span tag

--- src/components/CreateYourPizza/RadioButton.tsx
+++ src/components/CreateYourPizza/RadioButton.tsx
@@ -12,9 +12,7 @@ const RadioButton: React.FC<RadioButtonProps> = ({name, value, id, text, func, c
     <div className="button">
       <input type="radio" name={name} value={value} id={id} onClick={func} defaultChecked={checked} />
 
-      <label htmlFor={id}>
-        <span>{text}</span>
-      </label>
+      <label htmlFor={id}>{text}</label>
     </div>
   );
 };
